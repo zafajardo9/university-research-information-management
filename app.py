0742e9f756e0b569c63d8bae624a1d1cfb5287c1
zafajardo9 <zafajardo9.programming@gmail.com>
2023-08-18 16:27:16 +0800

    commit

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.get("/")
 def home():
-    return "Hello World!"
+    return render_template("index.html")  
+
